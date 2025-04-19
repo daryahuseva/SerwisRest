78fcdd8b6b51681eaa0ae1b24682c9ff6ad38b3e
daryahuseva <dorothy.dmitrievna@gmail.com>
2025-04-19 13:02:33 +0300

    Initial commit

---
+++ src/main/java/com/example/SerwisRest/SerwisRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SerwisRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerwisRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SerwisRest/SerwisRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SerwisRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwisRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
